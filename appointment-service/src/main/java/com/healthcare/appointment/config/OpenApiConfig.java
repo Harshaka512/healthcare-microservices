088b3ec40cd18ffc6316cbe1b444c66c5816cc37
Rashmi Praveesha <168809563+2rashmi@users.noreply.github.com>
2026-03-29 20:56:41 +0530

    Appointment-service
    Appointment-service

---
+++ appointment-service/src/main/java/com/healthcare/appointment/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.healthcare.appointment.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI appointmentServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Appointment Service API")
+                        .description("REST API for doctor appointments")
+                        .version("1.0"));
+    }
+}
